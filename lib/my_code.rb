83eeaaf77a3c92bffb04867c9dfba1ec2760528c
Anna Dybas <a.m.dybas@gmail.com>
2019-12-04 06:23:45 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,7 +2,7 @@
 def map(source_array)
   i = 0
   new = []
-  while i < source_array.length 
+  while i < source_array.length
     new << yield(source_array[i])
     i += 1
   end
@@ -17,7 +17,7 @@ def reduce(source_array, starting_point = nil)
     accume = source_array[0]
     i = 1
   end
-  while i < source_array.length 
+  while i < source_array.length
     accume = yield(accume, source_array[i])
     i += 1
   end
