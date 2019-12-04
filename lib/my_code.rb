5e671a87875b87b73c0bd2e7a256ebdbe215905d
Anna Dybas <a.m.dybas@gmail.com>
2019-12-04 06:11:32 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,6 +1,12 @@
 # Your Code Here
 def map(source_array)
-  
+  i = 0
+  new = []
+  while i < source_array.length do
+    new << yield(source_array[i])
+    i += 1
+  end
+  new
 end
 
 def reduce(source_array)
